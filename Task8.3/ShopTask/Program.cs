bcb20ddaec33fd93d3829bc79321eb80a18306d8
Vitalii <98163405+MementoMorj@users.noreply.github.com>
2022-06-28 21:32:35 +0300

    SubTask3

---
+++ Task8.3/ShopTask/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using ShopTask;
+var storage1 = new Storage(
+    new Product("moloko", 11, 12),
+    new Product("kava", 13, 24));
+var storage2 = new Storage(
+    new Product("moloko", 11, 12),
+    new Product("aefesf", 23445, 245),
+    new Product("sgsrgd", 243, 3534));
+
+foreach (var product in Storage.StorageExcept(storage1, storage2))
+{
+    Console.WriteLine(product);
+}
+
+foreach (var product in Storage.StorageIntersect(storage1, storage2))
+{
+    Console.WriteLine(product);
+}
+
+foreach (var product in Storage.StorageUnion(storage1, storage2))
+{
+    Console.WriteLine(product);
+}
